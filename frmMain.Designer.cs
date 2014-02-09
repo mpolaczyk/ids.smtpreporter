b902cef92f960d76da69444bfa6e67810e9ca3b3
Marcin Polaczyk <marcin.polaczyk@gmail.com>
2014-02-10 00:00:27 +0100

    Inital commit - tray application skeleton.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ids.smtpreport
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.context = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pokażToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabContainer = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolTipHello = new System.Windows.Forms.ToolTip(this.components);
+            this.context.SuspendLayout();
+            this.tabContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notify
+            // 
+            this.notify.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notify.BalloonTipTitle = "Powiadamiacz";
+            this.notify.ContextMenuStrip = this.context;
+            this.notify.Icon = ((System.Drawing.Icon)(resources.GetObject("notify.Icon")));
+            this.notify.Visible = true;
+            this.notify.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notify_MouseDoubleClick);
+            // 
+            // context
+            // 
+            this.context.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pokażToolStripMenuItem,
+            this.zamknijToolStripMenuItem});
+            this.context.Name = "context";
+            this.context.Size = new System.Drawing.Size(118, 48);
+            // 
+            // pokażToolStripMenuItem
+            // 
+            this.pokażToolStripMenuItem.Image = global::ids.smtpreport.Properties.Resources._16;
+            this.pokażToolStripMenuItem.Name = "pokażToolStripMenuItem";
+            this.pokażToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.pokażToolStripMenuItem.Text = "Pokaż";
+            this.pokażToolStripMenuItem.Click += new System.EventHandler(this.pokażToolStripMenuItem_Click);
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Image = global::ids.smtpreport.Properties.Resources._011;
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // tabContainer
+            // 
+            this.tabContainer.Controls.Add(this.tabPage1);
+            this.tabContainer.Controls.Add(this.tabPage2);
+            this.tabContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabContainer.Location = new System.Drawing.Point(0, 0);
+            this.tabContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabContainer.Name = "tabContainer";
+            this.tabContainer.SelectedIndex = 0;
+            this.tabContainer.Size = new System.Drawing.Size(571, 449);
+            this.tabContainer.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(563, 416);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Monitorowanie adresu IP";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(563, 418);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ustawienia";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolTipHello
+            // 
+            this.toolTipHello.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTipHello.ToolTipTitle = "Jestem tutaj!";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 449);
+            this.Controls.Add(this.tabContainer);
+            this.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Powiadamiacz";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            this.context.ResumeLayout(false);
+            this.tabContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notify;
+        private System.Windows.Forms.ContextMenuStrip context;
+        private System.Windows.Forms.ToolStripMenuItem pokażToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabContainer;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolTip toolTipHello;
+
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ids.smtpreport
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        public void SayHello()
+        {
+            notify.BalloonTipIcon = ToolTipIcon.Info;
+            notify.BalloonTipText = "Jestem tutaj !" + Environment.NewLine + "Kliknij prawym przyciskiem aby dowiedzieć się więcej.";
+            notify.BalloonTipTitle = "Powiadamiacz";
+            notify.ShowBalloonTip(3000);
+        }
+
+        private void ShowWindow()
+        {
+            this.Show();
+        }
+
+        private void HideWindow()
+        {
+            this.Hide();
+        }
+
+
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            SayHello();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                HideWindow();
+                SayHello();
+            }
+        }
+
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pokażToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowWindow();
+        }
+
+        private void notify_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowWindow();
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            HideWindow();
+        }
+    }
+}
